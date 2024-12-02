0aaafe3da0a2770661abe17a145b55c61b28ffd2
Jan Marten Regeling <jregeling@vitas.nl>
2024-12-02 17:57:52 +0100

    Add project files.

---
+++ advent-of-code-2024/Program.cs
@@ -0,0 +1,3 @@
+﻿using advent_of_code_2024.day01;
+
+day01.Run();
---
+++ advent-of-code-2024/day01/day01.cs
@@ -0,0 +1,35 @@
+﻿namespace advent_of_code_2024.day01;
+
+internal static class day01
+{
+    public static void Run()
+    {
+        var input = File.ReadAllText("day01/input").Split("\n").ToArray();
+
+        var list1 = new List<int>();
+        var list2 = new List<int>();
+
+        foreach (var line in input)
+        {
+            if (line.Length == 0)
+            {
+                continue;
+            }
+            list1.Add(int.Parse(line.Substring(0, line.IndexOf(' '))));
+            list2.Add(int.Parse(line.Substring(line.IndexOf(' '))));
+        }
+
+        list1 = [.. list1.OrderBy(x => x)];
+        list2 = [.. list2.OrderBy(x => x)];
+
+
+        var result = 0;
+        for (var i = 0; i < list1.Count; i++)
+        {
+            var distance = Math.Abs(list2[i] - list1[i]);
+            result += distance;
+        }
+
+        Console.WriteLine(result);
+    }
+}
